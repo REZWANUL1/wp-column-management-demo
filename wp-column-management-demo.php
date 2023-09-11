fdc72ccce22120630452f6f4c693a1e80c00b549
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-12 05:29:55 +0600

    adding files

---
+++ wp-column-management-demo.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+ * Plugin Name:       wp-column-management-demo
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rezwanul Haque
+ */

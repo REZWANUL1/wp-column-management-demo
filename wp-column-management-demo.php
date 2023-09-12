06de18afb51bfe8648ee0e03b19eff00090281c7
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-12 20:10:11 +0600

    adding post id functionality

--- wp-column-management-demo.php
+++ wp-column-management-demo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  * Plugin Name:       wp-column-management-demo
  * Description:       Handle the basics with this plugin.
@@ -7,3 +7,25 @@
  * Requires PHP:      7.2
  * Author:            Rezwanul Haque
  */
+
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+add_filter('manage_posts_columns', 'wp_column_posts');
+function wp_column_posts($columns)
+{
+   print_r($columns);
+   unset($columns['tags']);
+   unset($columns['author']);
+   $columns['tags'] = 'Tags';
+   $columns['author'] = 'Author';
+   $columns['id'] = __('Post Id', 'cd');
+   return $columns;
+}
+add_action('manage_posts_custom_column', 'wp_column_posts_data', 10, 2);
+
+function wp_column_posts_data($column, $post_id)
+{
+   echo $post_id;
+}
